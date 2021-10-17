6409921d2aab1018e7e66ec80e9443c49173435d
raulsposito <raulsposito@gmail.com>
2021-10-16 21:01:10 -0300

    add express server

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,53 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      show: false,
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 150) {
+        this.show = false
+      } else this.show = true
+    })
+    return () => {
+      window.removeEventListener('scroll')
+    }
+  },
+}
 </script>
+
+<style scoped>
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes fadeout {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+
+.fadeIn {
+  -webkit-animation: fadein 1s ease-in alternate;
+  -moz-animation: fadein 1s ease-in alternate;
+  animation: fadein 1s ease-in alternate;
+}
+
+.fadeOut {
+  -webkit-animation: fadeout 1s ease-out;
+  -moz-animation: fadeout 1s ease-out;
+  animation: fadeout 1s ease-out;
+}
+</style>
