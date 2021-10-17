e81947e2e379c88b7b80889b267e3f20ca9207e6
raulsposito <raulsposito@gmail.com>
2021-10-17 00:25:47 -0300

    add name on landing

--- components/Scroll.vue
+++ components/Scroll.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="fixed bottom-3 left-24 max-w-7xl mx-auto">
+        <p class="-mt-24 text-3xl font-bold text-black">
+          Ana Sposito
+        </p>
         <div class="text-right py-20">
-        <p class="downarrows text-lg font-semibold text-black">
+        <p class="text-lg font-semibold text-black">
           scroll down
         </p>
-        <p class="downarrows pt-6 text-xl font-semibold text-black">↓</p>
+        <p class="pt-6 text-xl font-semibold text-black">↓</p>
       </div>
     </div>
 </template>
